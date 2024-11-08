3983ac9755d153bc20b2de5fc1023b912905f83b
Christian Oliver Santarin <santarinchristian5@gmail.com>
2024-11-08 15:00:17 +0800

    Completed Assignment 2: Prime number checker

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -16,4 +16,27 @@ export class AppController {
       [a, b] = [b, a + b];
     }return sequence;
   }
+
+  //Assignment 2: Prime Number Checker
+
+  @Get('prime/:number')
+  checkPrime(@Param('number', ParseIntPipe)number: number){
+      const isPrime = this.isPrime(number);
+      return {isPrime};
+  }
+
+  //Prime number checker logic
+  isPrime(num: number){
+      if(num <= 1){
+        return false};
+      if(num == 2 || num == 3) {
+        return true};
+      if(num % 2 == 0 || num % 3 == 0) {
+        return false};
+        
+      for (let i = 5; i * i <= num; i += 6) {
+      if (num % i === 0 || num % (i + 2) === 0) return false;
+      }
+      return true;
+  }
 }
