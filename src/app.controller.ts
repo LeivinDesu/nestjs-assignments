52240b75194602a2d985ed0f8155b63a5b5bc901
Christian Oliver Santarin <santarinchristian5@gmail.com>
2024-11-08 14:49:29 +0800

    Completed Assignment 1: Fibonacci Sequence

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  
+  //Assignment 1: Fibonacci Sequence
+  @Get('fibonacci/:n')
+  fibonacci(@Param('n', ParseIntPipe)n: number){
+    let sequence = [];
+    let a = 0, b = 1;
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+    for(let i = 0; i < n; i++){
+      sequence.push(a);
+      [a, b] = [b, a + b];
+    }return sequence;
   }
 }
